09890b5d6669184e62e2d8be3d636906aecad179
divyashan3223 <divyashan3223@gmail.com>
2025-05-17 11:40:37 +0530

    food deliver front end

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,45 @@
+import { assets } from "../../assets/assets";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum eum
+            totam magnam corrupti in suscipit fuga impedit sed amet corporis?
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUGH</h2>
+          <ul>
+            <li>+1-345-248-9876</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 @Tomato.com - All Right Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
